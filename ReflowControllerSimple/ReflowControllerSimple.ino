7065a615ed34de98a721b5ba5e61c2588df0315a
Marcel Hecko <maco@blava.net>
2020-09-16 10:43:28 +0200

    update code

--- ReflowControllerSimple/ReflowControllerSimple.ino
+++ ReflowControllerSimple/ReflowControllerSimple.ino
@@ -1,6 +1,6 @@
 //
-// This version does now use display, buttons or other fancy stuff and only works as
-// a very fundamental thermostat - keeps desired temperature forever
+// This version does not use display, buttons or other fancy stuff and only works as
+// a very fundamental baker - reach TARGET_TEMP, tne turn FAN on and then end forever until reset
 //
 
 #include "max6675.h"
@@ -12,27 +12,30 @@
 #define PIN_TC_CS A1
 #define PIN_TC_DO 12
 #define PIN_TC_CLK 13
+#define FAN_PIN A0
 
-#define TARGET_TEMP 105
+#define TARGET_TEMP 250
 
 MAX6675 thermocouple(PIN_TC_CLK, PIN_TC_CS, PIN_TC_DO);
 
 void setup() {
   pinMode(PIN_HEATER, OUTPUT);
+  pinMode(FAN_PIN, OUTPUT);
+
   digitalWrite(PIN_HEATER, 0);
 
   Serial.begin(115200);
   Serial.println("begin");
 }
 
-float t;
+float t = 100;
 
 void loop() {
   t = readThermocouple();
-  if ((t < 15) || (t > 260)) {
+  if ((t < 5) || (t > 260)) {
     Serial.println("Kill heater - something wrong with temperature probe!");
     digitalWrite(PIN_HEATER, LOW);
-    delay(10000);
+    while (1) {};
   }
 
   Serial.print("target C: ");
@@ -41,25 +44,42 @@ void loop() {
   Serial.print("current C: ");
   Serial.print(round(t));
   Serial.print(" -> ");
-  
+
   if (t < TARGET_TEMP) {
     Serial.println("ON!");
     digitalWrite(PIN_HEATER, HIGH);
-    delay(500);
-    digitalWrite(PIN_HEATER, LOW);
-    delay(100);
   } else {
-    Serial.println("OFF!");
+    Serial.println("Temperature reached - turning OFF forever!");
+    fan_on();
     digitalWrite(PIN_HEATER, LOW);
-    delay(1000);
+    while (1) {
+      Serial.println("Temperature reached - turning OFF forever!");
+      t = readThermocouple();
+      Serial.print("Current C: ");
+      Serial.println(round(t));
+      if (t < 45) {
+        fan_off();
+      }
+    }
   }
+}
+
+void fan_on() {
+  Serial.println("Turning FAN ON!");
+  analogWrite(FAN_PIN, 255);
+}
+
+void fan_off() {
+  Serial.println("Turning FAN OFF!");
+  analogWrite(FAN_PIN, 0);
 }
 
 float readThermocouple() {
   digitalWrite(PIN_LCD_CS, HIGH);
   digitalWrite(PIN_TC_CS, LOW);
-  delay(1);
+  delay(10); // otherwise does not work!!
   float temperature = thermocouple.readCelsius();
+  delay(1000); // otherwise does not work!!
 
   if (temperature == NAN) {
     Serial.println("Unable to read temperature!");
